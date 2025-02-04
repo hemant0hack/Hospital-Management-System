27963fc5d0a31ad5727617f7c20b3cd62c06ecdb
Hemant Rathore <hemant0hack@gmail.com>
2025-02-04 23:38:02 +0530

    Update System.c
    Appointment

--- System.c
+++ System.c
@@ -5,7 +5,7 @@
 
 // Define structures for Patient, Doctor, and Appointment
 typedef struct {
-    int id;
+    char id[20];
     char name[100];
     int age;
     char gender[10];
@@ -14,16 +14,16 @@ typedef struct {
 } Patient;
 
 typedef struct {
-    int id;
+    char id[20];
     char name[100];
     char specialization[100];
     int experience;
 } Doctor;
 
 typedef struct {
-    int id;
-    int patientId;
-    int doctorId;
+    char id[20];
+    char patientId;
+    char doctorId;
     char appointmentDateTime[30];
 } Appointment;
 
@@ -45,7 +45,7 @@ void getCurrentDateTime(char* buffer) {
 // Function to add a new patient
 void addPatient() {
     printf("Enter Patient ID: ");
-    scanf("%d", &patients[patientCount].id);
+    scanf("%s", patients[patientCount].id);
     printf("Enter Patient Name: ");
     scanf("%s", patients[patientCount].name);
     printf("Enter Patient Age: ");
@@ -62,32 +62,13 @@ void addPatient() {
 // Function to view all patients
 void viewPatients() {
     for (int i = 0; i < patientCount; i++) {
-        printf("ID: %d, Name: %s, Age: %d, Gender: %s, Disease: %s, Admission DateTime: %s\n", patients[i].id, patients[i].name, patients[i].age, patients[i].gender, patients[i].disease, patients[i].admissionDateTime);
+        printf("ID: %s, Name: %s, Age: %d, Gender: %s, Disease: %s, Admission DateTime: %s\n", patients[i].id, patients[i].name, patients[i].age, patients[i].gender, patients[i].disease, patients[i].admissionDateTime);
     }
 }
-
-// Function to delete a patient by ID
-void deletePatient() {
-    int id;
-    printf("Enter Patient ID to delete: ");
-    scanf("%d", &id);
-    for (int i = 0; i < patientCount; i++) {
-        if (patients[i].id == id) {
-            for (int j = i; j < patientCount - 1; j++) {
-                patients[j] = patients[j + 1];
-            }
-            patientCount--;
-            printf("Patient deleted successfully!\n");
-            return;
-        }
-    }
-    printf("Patient not found!\n");
-}
-
 // Function to add a new doctor
 void addDoctor() {
     printf("Enter Doctor ID: ");
-    scanf("%d", &doctors[doctorCount].id);
+    scanf("%s", &doctors[doctorCount].id);
     printf("Enter Doctor Name: ");
     scanf("%s", doctors[doctorCount].name);
     printf("Enter Doctor Specialization: ");
@@ -101,36 +82,19 @@ void addDoctor() {
 // Function to view all doctors
 void viewDoctors() {
     for (int i = 0; i < doctorCount; i++) {
-        printf("ID: %d, Name: %s, Specialization: %s, Experience: %d years\n", doctors[i].id, doctors[i].name, doctors[i].specialization, doctors[i].experience);
+        printf("ID: %s, Name: %s, Specialization: %s, Experience: %d years\n", doctors[i].id, doctors[i].name, doctors[i].specialization, doctors[i].experience);
     }
 }
 
-// Function to delete a doctor by ID
-void deleteDoctor() {
-    int id;
-    printf("Enter Doctor ID to delete: ");
-    scanf("%d", &id);
-    for (int i = 0; i < doctorCount; i++) {
-        if (doctors[i].id == id) {
-            for (int j = i; j < doctorCount - 1; j++) {
-                doctors[j] = doctors[j + 1];
-            }
-            doctorCount--;
-            printf("Doctor deleted successfully!\n");
-            return;
-        }
-    }
-    printf("Doctor not found!\n");
-}
 
 // Function to add a new appointment
 void addAppointment() {
     printf("Enter Appointment ID: ");
-    scanf("%d", &appointments[appointmentCount].id);
+    scanf("%s", &appointments[appointmentCount].id);
     printf("Enter Patient ID: ");
-    scanf("%d", &appointments[appointmentCount].patientId);
+    scanf("%s", &appointments[appointmentCount].patientId);
     printf("Enter Doctor ID: ");
-    scanf("%d", &appointments[appointmentCount].doctorId);
+    scanf("%s", &appointments[appointmentCount].doctorId);
     getCurrentDateTime(appointments[appointmentCount].appointmentDateTime);
     appointmentCount++;
     printf("Appointment added successfully!\n");
@@ -139,7 +103,7 @@ void addAppointment() {
 // Function to view all appointments
 void viewAppointments() {
     for (int i = 0; i < appointmentCount; i++) {
-        printf("ID: %d, Patient ID: %d, Doctor ID: %d, Appointment DateTime: %s\n", appointments[i].id, appointments[i].patientId, appointments[i].doctorId, appointments[i].appointmentDateTime);
+        printf("ID: %s, Patient ID: %s, Doctor ID: %s, Appointment DateTime: %s\n", appointments[i].id, appointments[i].patientId, appointments[i].doctorId, appointments[i].appointmentDateTime);
     }
 }
 
@@ -147,15 +111,13 @@ void viewAppointments() {
 int main() {
     int choice;
     while (1) {
-        printf("1. Add Patient\n");
+        printf("\n1. Add Patient\n");
         printf("2. View Patients\n");
-        printf("3. Delete Patient\n");
-        printf("4. Add Doctor\n");
-        printf("5. View Doctors\n");
-        printf("6. Delete Doctor\n");
-        printf("7. Add Appointment\n");
-        printf("8. View Appointments\n");
-        printf("9. Exit\n");
+        printf("3. Add Doctor\n");
+        printf("4. View Doctors\n");
+        printf("5. Add Appointment\n");
+        printf("6. View Appointments\n");
+        printf("7. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
         switch (choice) {
@@ -166,24 +128,18 @@ int main() {
                 viewPatients();
                 break;
             case 3:
-                deletePatient();
-                break;
-            case 4:
                 addDoctor();
                 break;
-            case 5:
+            case 4:
                 viewDoctors();
                 break;
-            case 6:
-                deleteDoctor();
-                break;
-            case 7:
+            case 5:
                 addAppointment();
                 break;
-            case 8:
+            case 6:
                 viewAppointments();
                 break;
-            case 9:
+            case 7:
                 exit(0);
             default:
                 printf("Invalid choice!\n");
